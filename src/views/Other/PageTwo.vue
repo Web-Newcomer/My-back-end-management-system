0c850d4b8a8d2be127133463435570aac6459f47
Web-Newcomer <83285056+Web-Newcomer@users.noreply.github.com>
2021-12-27 01:22:43 +0800

    修复bug
    修复bug

---
+++ src/views/Other/PageTwo.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-descriptions title="自定义样式列表" :column="3" border>
+    <el-descriptions-item label="用户名" label-class-name="my-label" content-class-name="my-content">kooriookami</el-descriptions-item>
+    <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
+    <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
+    <el-calendar v-model="value"> </el-calendar>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学校</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="联系地址" :contentStyle="{ 'text-align': 'right' }">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+  </el-descriptions>
+</template>
+
+<style>
+.my-label {
+  background: #e1f3d8;
+}
+
+.my-content {
+  background: #fde2e2;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      value: new Date()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/VideoManage/VideoManage.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-container style="height: 500px; border: 1px solid #eee">
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>李小龙</span>
+      </el-header>
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="日期" width="140"> </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+          <el-table-column prop="address" label="地址"> </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    const item = {
+      date: '2021-12-12',
+      name: '李小龙',
+      address: '北京市海淀区西北旺东路10号院西区9号楼'
+    }
+    return {
+      tableData: Array(20).fill(item)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
